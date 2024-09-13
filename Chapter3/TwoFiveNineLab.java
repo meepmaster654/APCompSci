e56a1f2da272e4f5ae8baa6403705ecf4acee44f
WillLeasure <121632307+meepmaster654@users.noreply.github.com>
2024-09-13 11:26:37 -0500

    Update 9/13

---
+++ Chapter3/TwoFiveNineLab.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class TwoFiveNineLab {
+  public static void main(String[] args) {
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Input a Fahrenheit temperature:");
+    FToCTemp twoFive = new FToCTemp(userInput.nextDouble());
+    System.out.println("Input the base sides of your pyramid:");
+    Pyramids twoSix = new Pyramids(userInput.nextInt());
+    System.out.println("Input the numerator of your fraction, click enter, then enter the denominator");
+    ImproperFractions twoSever = new ImproperFractions(userInput.nextInt(),userInput.nextInt());
+  }
+}
+class FToCTemp {
+  public FToCTemp(double userTemp) {
+    double convertedTemp = (userTemp-(double)32)/1.8;
+    System.out.println(userTemp +" degrees Fahrenheit\n     is equal to\n"+ convertedTemp + "degrees Celsius");
+  }
+}
+class Pyramids {
+  public Pyramids(int userSides) {
+    int verticies = userSides + 1;
+    int edges = userSides * 2;
+    int faces = userSides + 1;
+    System.out.println("You input a pyramid with a base of "+ userSides + " sides.\nThe computer says, \"This pyramid has " + verticies + " faces, " + edges + " edges, and " + faces + " \nvertices.\"");
+  }
+}
+
+class ImproperFractions {
+  public ImproperFractions(int topNum, int botNum){
+    int newTop, whole;
+    whole =  topNum%botNum;
+    newTop =  topNum-whole*botNum;
+    System.out.println(topNum + "/" + botNum+" = " + whole +" "+newTop + "/" + botNum);
+  }
+}
